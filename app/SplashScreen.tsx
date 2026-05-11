b190baa90ce6d80e982106a8bbbd617918047d03
Shaahzad <shehzadmemon147@gmail.com>
2026-05-11 22:16:09 +0500

    learn react native

---
+++ app/SplashScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Splash from './components/Splash'
+
+const SplashScreen = () => {
+  return (
+    <Splash/>
+  )
+}
+
+export default SplashScreen
--- app/index.tsx
+++ app/index.tsx
@@ -1,52 +1,18 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import Colors from '@/constants/Colors'
-import { StatusBar } from 'expo-status-bar'
-import { FilterOptions, TASK } from '@/constants/tasks'
-import Header from '@/components/Header'
-import DateSelector from '@/components/DateSelector'
-import FilterTabs from '@/components/FilterTabs'
-import TaskCards from '@/components/TaskCards'
+import { Link } from 'expo-router'
+import React from 'react'
+import { Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-const index = () => {
-  const inset = useSafeAreaInsets()
-  const [activeFilter, setActiveFilter] = useState<FilterOptions>("All")
+const Index = () => {
   return (
-    <View style={[styles.container, {paddingTop: inset.top}]}>
-      <StatusBar style='light'/>
-      <FlatList
-      data={TASK}
-      keyExtractor={item => item.id}
-      renderItem={({item}) => <TaskCards task={item}/>}
-      ListHeaderComponent={
-        <>
-        {/* Header */}
-        <Header/>
-        {/* DateSelector */}
-        <DateSelector/>
-        {/* FiltersTabs */}
-        <FilterTabs selected={activeFilter} onSelect={setActiveFilter}/>
-        </>
-      }
-      contentContainerStyle={styles.list}
-      showsVerticalScrollIndicator={false}
-      />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View className="flex-1 items-center justify-center">
+        <Link href="/SplashScreen">
+          <Text>Go to Splash Screen</Text>
+        </Link>
+      </View>
+    </SafeAreaView>
   )
 }
 
-export default index
-
-const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    backgroundColor: Colors.background
-  },
-  Text: {
-    color: Colors.textPrimary
-  },
-  list: {
-    paddingBottom: 24
-  }
-})
+export default Index
